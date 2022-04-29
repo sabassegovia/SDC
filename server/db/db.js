1a8b21171de78439e2a5f73cc81da2888040ffc0
Sabas Segovia <sasegovia@ucdavis.edu>
2022-04-29 01:02:14 -0700

    first commit since typ0, still looking

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,13 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: '54.183.12.42',
+  host: '13.57.47.161',
   user: 'test_user',
   port: 5432,
   password: 'password',
   database: 'test'
 })
+
 pool.connect((err, res) => {
   if (err) {
     console.log(err);
