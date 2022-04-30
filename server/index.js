3310ac40ccf9ea898ec3b5f909f1247b4335c46a
Sabas Segovia <sasegovia@ucdavis.edu>
2022-04-30 15:41:18 -0700

    feat: loader.io token

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ app.get(`/products/:product_id/related`, (req, res) => {
   })
 })
 
-app.get('/loaderio-969be4013b423bf9eacddcd6d00b8919/', (req, res) => {
+app.get('/loaderio-969be4013b423bf9eacddcd6d00b8919.txt', (req, res) => {
   res.status(200).send('done');
 })
 app.listen(port, () => {
