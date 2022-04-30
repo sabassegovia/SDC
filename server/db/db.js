b183d0b4cbb87fc190b7067b40aec7cfe89d6e13
Sabas Segovia <sasegovia@ucdavis.edu>
2022-04-29 17:04:52 -0700

    pushing new db deployment

--- server/db/db.js
+++ server/db/db.js
@@ -5,12 +5,19 @@ const { Pool } = require('pg');
 //13.57.47.161
 //13.57.193.100
 const pool = new Pool({
-  host: '54.183.12.42',
+  host: '54.193.237.7',
   user: 'test_user',
   port: 5432,
-  password: 'password',
+  password: 'testpassword',
   database: 'test'
 })
+// const pool = new Pool({
+//   host: '52.53.177.10',
+//   user: 'test_user',
+//   port: 5432,
+//   password: 'password',
+//   database: 'test'
+// })
 
 pool.connect((err, res) => {
   if (err) {
